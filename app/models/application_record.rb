0a33caa385f9cfdf83beb34de0699b3311be8de2
David Harmon <dharmon202@gmail.com>
2021-04-12 19:57:22 -0400

    Add CoinBank::Currency model and table
    This is the table that will be used to store information on
    Cryptocurrencies.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
+# require coin_bank as our classes are
+# namespaced inside of it
+require 'coin_bank'
+
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
