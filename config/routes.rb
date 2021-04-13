87277a24d4b6a88fbd52bcf0fdffda189ac94eed
David Harmon <dharmon202@gmail.com>
2021-04-12 21:51:25 -0400

    Add CurrenciesController and #new template

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: :coin_bank do
+    resources :currencies, as: :coin_bank_currencies
+  end
 end
