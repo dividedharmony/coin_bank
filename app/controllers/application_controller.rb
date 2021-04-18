88e6ab8bda26daf9fd042bffc4ebe15b3f697e3b
David Harmon <dharmon202@gmail.com>
2021-04-18 06:27:06 -0400

    Rescue from RecordNotFound exceptions in ApplicationController
    ...and return a simple 404 message. Later commits may add a
    custom 404 page but this message works for now.
    
    If other controllers need to change this behavior, they can
    simply override the private #record_not_found method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found
+    render plain: "404 Not Found", status: 404
+  end
 end
