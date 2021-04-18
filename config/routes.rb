ab2456c98372742e371ee354f942de1509329866
David Harmon <dharmon202@gmail.com>
2021-04-18 07:17:36 -0400

    Add HomeController#index as app root page
    This commit adds a very simple homepage just so our app can have
    a root path (which devise needs).

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+
   scope module: :coin_bank do
     resources :currencies, as: :coin_bank_currencies
   end
