e16a8a6f4abaaee1fb1a688b048b4673f1c71849
David Harmon <dharmon202@gmail.com>
2021-04-27 00:53:27 -0400

    Add coin_bank_currencies.fiat boolean column
    A "fiat" currency is government-issued, as opposed to decentralized
    cryptocurrencies.
    
    Within the CoinBank app, a user can use fiat currencies to cover
    transaction fees and buy units of cryptocurrencies.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_26_153837) do
+ActiveRecord::Schema.define(version: 2021_04_27_044939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_04_26_153837) do
     t.string "logo_url", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "fiat", default: false, null: false
   end
 
   create_table "coin_bank_users", force: :cascade do |t|
