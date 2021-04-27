1e6bed6b028c7c0f0aa1498e7919382f949295bb
David Harmon <dharmon202@gmail.com>
2021-04-27 00:47:02 -0400

    Add CoinBank::User#current_balance association
    This association uses the Balance.latest_per_currency scope to
    deliver all of a user's balances for each currency they have
    a balance associated with.

--- spec/models/coin_bank/user_spec.rb
+++ spec/models/coin_bank/user_spec.rb
@@ -5,6 +5,36 @@ require 'rails_helper'
 RSpec.describe CoinBank::User, type: :model do
   describe "associations" do
     it { is_expected.to have_many(:balances).class_name("CoinBank::Balance") }
+
+    describe "#current_balances" do
+      subject { user.current_balances }
+
+      let(:user) { create(:user) }
+      let(:other_user) { create(:user) }
+
+      let(:currency_1) { create(:currency) }
+      let(:currency_2) { create(:currency) }
+      let(:currency_3) { create(:currency) }
+      let(:currency_4) { create(:currency) }
+
+      # Only one currency and one user
+      let!(:balance_1) { create(:balance, user: user, currency: currency_1) }
+
+      # Two balances, should only include latest
+      let!(:balance_2) { create(:balance, user: user, currency: currency_2) }
+      let!(:balance_3) { create(:balance, user: user, currency: currency_2) }
+
+      # Two users, one currency
+      let!(:balance_4) { create(:balance, user: user, currency: currency_3) }
+      let!(:balance_5) { create(:balance, user: other_user, currency: currency_3) }
+
+      # A currency our user has never used
+      let!(:balance_6) { create(:balance, user: other_user, currency: currency_4) }
+
+      it "only includes the latest balance for the given user" do
+        expect(subject).to contain_exactly(balance_1, balance_3, balance_4)
+      end
+    end
   end
 
   describe "validations" do
