9fcc414c0bffb3c16dcaa321c1f51813b7f7dc54
David Harmon <dharmon202@gmail.com>
2021-05-21 16:42:20 -0400

    Add CoinbaseIntegration::Resource
    This class wraps a parsed JSON respnse from the Coinbase API.
    If the body of the response is an array then the Resource
    instance is an Enumerable.

---
+++ app/services/coinbase_integration/resource.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module CoinbaseIntegration
+  class Resource
+    include Enumerable
+
+    def initialize(body)
+      @body = body
+    end
+
+    attr_reader :body
+    delegate :each, :[], :slice, to: :data
+
+    def data
+      body["data"]
+    end
+
+    def pagination
+      body["pagination"]
+    end
+
+    def errors
+      body["errors"]
+    end
+  end
+end
