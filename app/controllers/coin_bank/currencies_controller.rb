1bb8e1cf6f7e0df3dc8ea1e2325cd9ab364b444a
David Harmon <dharmon202@gmail.com>
2022-02-14 10:55:08 -0500

    Add CurrenciesController#create

--- app/controllers/coin_bank/currencies_controller.rb
+++ app/controllers/coin_bank/currencies_controller.rb
@@ -9,5 +9,17 @@ module CoinBank
     def show
       @currency = CoinBank::Currency.find_by!(slug: params[:id])
     end
+
+    def create
+      result = CoinBank::CreateCurrency.(params)
+      if result.success?
+        currency = result.value!
+        flash[:success] = "Successfully saved the #{currency.name} cryptocurrency!"
+        redirect_to coin_bank_currency_path(currency.slug)
+      else
+        flash[:error] = result.message
+        redirect_to action: :new
+      end
+    end
   end
 end
