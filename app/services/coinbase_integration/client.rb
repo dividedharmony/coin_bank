46a0046d1f03dfab0b756d398bad7015393d61ba
David Harmon <dharmon202@gmail.com>
2022-02-15 03:32:31 -0500

    Add pagination params to CoinbaseIntegration::Client#accounts
    There are a lot of accounts and all of them need to be
    iterated over.

--- app/services/coinbase_integration/client.rb
+++ app/services/coinbase_integration/client.rb
@@ -9,8 +9,8 @@ module CoinbaseIntegration
 
     include Results::Methods
 
-    def accounts
-      request("/v2/accounts")
+    def accounts(starting_after: nil)
+      request("/v2/accounts", starting_after: starting_after)
     end
 
     def transactions(account_uuid, starting_after_uuid: nil)
