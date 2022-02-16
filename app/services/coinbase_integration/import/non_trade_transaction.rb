3b68aaa359dada82df824adfcea7bd78a9a79718
David Harmon <dharmon202@gmail.com>
2022-02-15 20:46:38 -0500

    Add CoinbaseIntegration::Import::NonTradeTransaction

---
+++ app/services/coinbase_integration/import/non_trade_transaction.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module CoinbaseIntegration
+  module Import
+    class NonTradeTransaction
+      def initialize(currencies)
+        @currencies = currencies
+      end
+
+      def import!(user, raw_transaction)
+        transaction_struct = TransactionStruct.new(raw_transaction)
+        CoinBank::Transaction.new(
+          user: user,
+          from_currency: currencies.fetch(transaction_struct.from_currency_symbol),
+          from_amount: transaction_struct.from_amount,
+          to_currency: currencies.fetch(transaction_struct.to_currency_symbol),
+          to_amount: transaction_struct.to_amount,
+          exchange_rate: transaction_struct.exchange_rate,
+          transacted_at: transaction_struct.transacted_at,
+          coinbase_uuid: raw_transaction.fetch('id')
+        )
+      end
+
+      private
+
+      attr_reader :currencies
+    end
+  end
+end
