c2795d7b94bc3d246efa78cf95ad2cd30577e7c0
David Harmon <dharmon202@gmail.com>
2022-02-18 07:33:59 -0500

    Do not load Dotenv in production
    Just use ENV variables loaded by Heroku in production.

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,14 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+unless Rails.env.production?
+  Dotenv::Railtie.load
+end
 
 module CoinBank
   class Application < Rails::Application
@@ -18,7 +20,7 @@ module CoinBank
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # config.time_zone = 'Central Time (US & Canada)'
+    # config.eager_load_paths << Rails.root.join('extras')
   end
 end
