4c03702d0f3c56bc250ba05c08f156efcd43eda4
David Harmon <dharmon202@gmail.com>
2022-02-17 21:08:34 -0500

    Ensure production has no viable routes
    This will prevent malicious requests from exploiting the server.

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
 
-  devise_for :users, class_name: "CoinBank::User"
+  # effectively headless in production
+  unless Rails.env.production?
+    root to: "home#index"
 
-  scope module: :coin_bank do
-    resources :currencies, as: :coin_bank_currencies
+    devise_for :users, class_name: "CoinBank::User"
+
+    scope module: :coin_bank do
+      resources :currencies, as: :coin_bank_currencies
+    end
   end
 end
