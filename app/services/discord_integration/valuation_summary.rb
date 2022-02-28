e6c58f2c01081dd922a01f179db68a2199190c6d
David Harmon <dharmon202@gmail.com>
2022-02-28 15:16:11 -0500

    Add currency-specific alert_threshold to ValuationSummary message

--- app/services/discord_integration/valuation_summary.rb
+++ app/services/discord_integration/valuation_summary.rb
@@ -20,7 +20,7 @@ module DiscordIntegration
       def formatted_stats
         ValuationStatus.all.map do |valuation_status|
           alert = valuation_status.alert? ? '**ALERT**' : '.....'
-          "#{alert} #{valuation_status.currency_symbol} - #{valuation_status.percent_of_target}   (#{valuation_status.actual_value}/#{valuation_status.target_value})"
+          "#{alert} #{valuation_status.currency_symbol} - **#{valuation_status.percent_of_target}**/#{valuation_status.alert_threshold} -- (#{valuation_status.actual_value}/#{valuation_status.target_value})"
         end.join("\n")
       end
     end
